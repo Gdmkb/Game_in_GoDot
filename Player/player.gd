753d1263c12ec599799456948c422111154ab9d5
Gdmkb <akhmetshin-1999@inbox.ru>
2026-05-07 23:16:28 +0300

    Finish lesson #5
    Finish lesson #5

--- Player/player.gd
+++ Player/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 @onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
