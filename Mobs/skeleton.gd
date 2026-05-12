ce88add8064928a7fe6386736130e3eafe6bce19
Gdmkb <akhmetshin-1999@inbox.ru>
2026-05-12 23:19:37 +0300

    Lesson #8 (unfinished)
    void combo1 is not missing

--- Mobs/skeleton.gd
+++ Mobs/skeleton.gd
@@ -1,11 +1,10 @@
 extends CharacterBody2D
 
-
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 var chase = false
-
 var speed = 100
+@onready var anim = $AnimatedSprite2D
+var alive = true 
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -13,11 +12,17 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 	var player = $"../../Player/Player"
 	var direction = (player.global_position - self.global_position).normalized()
-	if chase == true:
-		velocity.x = direction.x * speed
-	else:
-		velocity.x = 0
-		
+	if alive == true:
+		if chase == true:
+			velocity.x = direction.x * speed
+			anim.play("Run")
+		else:
+			velocity.x = 0
+			anim.play("Idle")
+		if direction.x < 0:
+			$AnimatedSprite2D.flip_h = true
+		else: 
+			$AnimatedSprite2D.flip_h = false
 	move_and_slide()
 
 func _on_detector_body_entered(body):
@@ -28,3 +33,21 @@ func _on_detector_body_entered(body):
 func _on_detector_body_exited(body):
 	if body.name == "Player":
 		chase = false 
+
+
+func _on_death_body_entered(body):
+	if body.name == "Player":
+		body.velocity.y -= 300
+		death()
+		
+func _on_death_2_body_entered(body):
+	if body.name == "Player":
+		if alive == true:
+			body.health -= 40
+		death()
+	
+func  death ():
+	alive = false 
+	anim.play("Death")
+	await anim.animation_finished
+	queue_free()
--- Player/player.gd
+++ Player/player.gd
@@ -1,40 +1,116 @@
 extends CharacterBody2D
 
+enum {
+	MOVE,
+	ATTACK,
+	ATTACK2,
+	BLOCK,
+	SLIDE
+}
 
-const SPEED = 300.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var anim = $AnimatedSprite2D
+@onready var animPlayer = $AnimationPlayer
+var health = 100
+var gold = 0
+var state = MOVE
+var run_speed = 1
+var combo = false
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state()
+		ATTACK:
+			attack_state()
+		ATTACK2:
+			attack2_state()
+		BLOCK:
+			block_state()
+		SLIDE:
+			slide_state()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		anim.play("Jump")
+	if velocity.y > 0:
+		animPlayer.play("Fall")
+		
+	if health <= 0:
+		health = 0
+		animPlayer.play("Death")
+		await animPlayer.animation_finished
+		queue_free()
+		get_tree().change_scene_to_file("res://menu.tscn")
+		
+	move_and_slide()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+func move_state ():
+	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED * run_speed
 		if velocity.y == 0:
-			anim.play("Run")
+			if run_speed == 1:
+				animPlayer.play("Walk")
+			else:
+				animPlayer.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if velocity.y == 0:
-			anim.play("Idle")
-		
+			animPlayer.play("Idle")
 	if direction == -1:
-		$AnimatedSprite2D.flip_h = true
+		anim.flip_h = true
 	elif direction == 1:
-		$AnimatedSprite2D.flip_h = false
+		anim.flip_h = false
+	if Input.is_action_pressed("run"):
+		run_speed = 2
+	else:
+		run_speed = 1
 		
-	if velocity.y > 0:
-		anim.play("Fall")
-	move_and_slide()
+	if Input.is_action_pressed("block"):
+		if velocity.x == 0:
+			state = BLOCK
+		else:
+			state = SLIDE
+			
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+# block
+func block_state():
+	velocity.x = 0
+	animPlayer.play("Block")
+	if Input.is_action_just_released("block"):
+		state = MOVE
+
+# slide
+func slide_state():
+	animPlayer.play("Slide")
+	await animPlayer.animation_finished
+	state = MOVE
+
+# attack
+func attack_state():
+	if Input.is_action_pressed("attack") and combo == true:
+		state = ATTACK2
+	velocity.x = 0
+	animPlayer.play("Attack")
+	await animPlayer.animation_finished
+	state = MOVE
+	
+func attack2_state ():
+	animPlayer.play("Attack2")
+	await animPlayer.animation_finished
+	state = MOVE
+	
+func combo1 ():
+	combo = true 
+	await animPlayer.animation_finished
+	combo = false
+	
+	
