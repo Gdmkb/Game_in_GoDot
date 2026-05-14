74faa6c2d2f1bb8571dd7abaf003735fd9519181
Gdmkb <akhmetshin-1999@inbox.ru>
2026-05-14 18:55:01 +0300

    Lesson #9 (finish)
    Lesson #9 (finish)

--- level.gd
+++ level.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 @onready var light = $DirectionalLight2D
 @onready var pointlight = $PointLight2D
+@onready var day_text = $"CanvasLayer/Day Text"
+@onready var animPlayer = $CanvasLayer/AnimationPlayer
+
 enum {
 	MORNING,
 	DAY,
@@ -10,16 +13,14 @@ enum {
 }
 
 var state = MORNING
+var day_count: int 
 
 func _ready():
 	light.enabled = true
+	day_count = 1
+	set_day_text()
+	day_text_fade()
 
-func _process(delta):
-	match state:
-		MORNING:
-			morning_state()
-		EVENING:
-			evening_state()
 			
 func morning_state ():
 	var tween = get_tree().create_tween()
@@ -34,7 +35,23 @@ func evening_state ():
 	tween1.tween_property(pointlight, "energy", 1.5, 20)
 
 func _on_day_night_timeout():
+	match state:
+		MORNING:
+			morning_state()
+		EVENING:
+			evening_state()
 	if state < 3:
 		state += 1 
 	else:
 		state = MORNING
+		day_count += 1
+		set_day_text()
+		day_text_fade()
+
+func day_text_fade ():
+	animPlayer.play("day_text_fade_in")
+	await get_tree().create_timer(3).timeout
+	animPlayer.play("day_text_fade_out")
+	
+func set_day_text ():
+	day_text.text = "DAY" + str(day_count)
